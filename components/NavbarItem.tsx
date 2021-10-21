6f9f11b6ff876eb5c7f5fcb1566b38038c58f4fc
Felix Pacheco <devfex.19@gmail.com>
2021-10-21 18:55:03 -0400

    Set active link styles

--- components/NavbarItem.tsx
+++ components/NavbarItem.tsx
@@ -1,25 +1,29 @@
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 type Props = {
     title: string,
-    // link: string,
+    link: string,
     icon: string,
     titleTranslated: string
 };
 
 const NavbarItem = ({
-    title, icon, titleTranslated,
+    title, icon, link, titleTranslated,
 }: Props): JSX.Element => {
-    const router = useRouter();
-    const isEnglishLanguage = router.locale === 'en';
+    const { locale, pathname } = useRouter();
+    const isEnglishLanguage = locale === 'en';
+    const isActive = pathname === link;
 
     return (
-        <div className="flex flex-col items-center h-full py-1 text-gray-100 transition duration-500 cursor-pointer ease hover:text-pink-700">
-            <span className="text-lg mobileXl:hidden"><i aria-hidden className={icon} /></span>
-            <span className="hidden px-4 text-sm text-left mobile:block mobileXl:text-base">
-                {isEnglishLanguage ? titleTranslated : title}
-            </span>
-        </div>
+        <Link href={link}>
+            <a className={`flex flex-col items-center h-full py-1 transition duration-500 cursor-pointer ease hover:text-pink-700 ${isActive ? 'text-pink-700' : 'text-gray-100'}`}>
+                <span className="text-lg mobileXl:hidden"><i aria-hidden className={icon} /></span>
+                <span className="hidden px-4 text-sm text-left mobile:block mobileXl:text-base">
+                    {isEnglishLanguage ? titleTranslated : title}
+                </span>
+            </a>
+        </Link>
     );
 };
 
