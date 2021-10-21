fa0b5233b2ce6da2f710ec9526c183064e4bf5fa
Felix Pacheco <devfex.19@gmail.com>
2021-10-21 18:24:57 -0400

    Refactor Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,8 @@
+import PageLayout from '@components/PageLayout';
+import NavbarContainer from '@components/NavbarContainer';
+import Welcome from '@components/Welcome';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import Header from '../components/Header';
-import NavbarContainer from '../components/NavbarContainer';
-import PrincipalBanner from '../components/PrincipalBanner';
-import NextTip from '../components/NextTip';
-import TypescriptTip from '../components/TypescriptTip';
-import CypressTip from '../components/CypressTip';
-import Training from '../components/Training';
-import Experiences from '../components/Experiences';
-import Skills from '../components/Skills';
-import About from '../components/About';
-import Thanks from '../components/Thanks';
-import Contact from '../components/Contact';
-import Footer from '../components/Footer';
+import Layout from '@components/Layout';
 
 export const getStaticProps = async ({ locale }: { locale: never }) => ({
     props: {
@@ -20,21 +11,14 @@ export const getStaticProps = async ({ locale }: { locale: never }) => ({
 });
 
 const Index = (): JSX.Element => (
-    <>
-        <Header />
-        <NavbarContainer />
-        <PrincipalBanner />
-        <Training />
-        <NextTip />
-        <Experiences />
-        <TypescriptTip />
-        <Skills />
-        <CypressTip />
-        <About />
-        <Thanks />
-        <Contact />
-        <Footer />
-    </>
+    <PageLayout extraClasses="h-screen home-bg">
+        <>
+            <NavbarContainer />
+            <Layout>
+                <Welcome />
+            </Layout>
+        </>
+    </PageLayout>
 );
 
 export default Index;
