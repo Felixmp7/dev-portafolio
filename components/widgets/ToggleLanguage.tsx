e12d1188dc9803f02e2aaa507eb009da95f80c7a
Felix Pacheco <devfex.19@gmail.com>
2021-10-21 18:54:13 -0400

    Fix error in ToggleLanguage

--- components/widgets/ToggleLanguage.tsx
+++ components/widgets/ToggleLanguage.tsx
@@ -1,24 +1,19 @@
-/* eslint-disable @next/next/link-passhref */
 import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
 const ToggleLanguage = (): JSX.Element => {
-    const router = useRouter();
-    const isEnglishLanguage = router.locale === 'en';
+    const { locale, pathname } = useRouter();
+    const isEnglishLanguage = locale === 'en';
     const activeClasses = 'text-gray-600 font-semibold bg-white';
 
     return (
         <div className="text-xs">
-            <Link href="/" locale="en">
-                <span className={`toggleLangugage ease mr-1 ${isEnglishLanguage && activeClasses}`}>
-                    EN
-                </span>
+            <Link href={pathname} locale="en">
+                <a className={`toggleLangugage ease mr-1 ${isEnglishLanguage && activeClasses}`}>EN</a>
             </Link>
-            <Link href="/" locale="es">
-                <span className={`toggleLangugage ease ${!isEnglishLanguage && activeClasses}`}>
-                    ES
-                </span>
+            <Link href={pathname} locale="es">
+                <a className={`toggleLangugage ease ${!isEnglishLanguage && activeClasses}`}>ES</a>
             </Link>
         </div>
     );
