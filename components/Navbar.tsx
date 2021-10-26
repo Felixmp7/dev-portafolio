e6373470db6b4f27a27fb1f7f1c8f3dfc33750ff
Felix Pacheco <devfex.19@gmail.com>
2021-10-26 11:05:29 -0400

    Edit bg opacity of navbars

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,29 @@
 import SocialMedia from '@widgets/SocialMedia';
 import { NavbarItemsProps } from 'types';
+import { useRouter } from 'next/router';
 import NavbarItem from './NavbarItem';
 import ToggleLanguage from './widgets/ToggleLanguage';
 
-const Navbar = ({ navbarItems }: NavbarItemsProps): JSX.Element => (
-    <header className="fixed top-0 z-50 w-full px-8 py-2 bg-black border-b-2 border-black bg-opacity-20 border-opacity-10 desktop:px-0">
-        <nav className="flex items-center justify-between h-full mx-auto containerXl">
-            <div className="flex w-auto justify-evenly">
-                {navbarItems.map((item) => (
-                    <NavbarItem key={item.id} {...item} />
-                ))}
-            </div>
-            <div className="flex items-center">
-                <ToggleLanguage />
-                <div className="flex ml-2 justify-evenly w-28">
-                    <SocialMedia linkClasses="text-xl" />
+const Navbar = ({ navbarItems }: NavbarItemsProps): JSX.Element => {
+    const { locale, pathname } = useRouter();
+
+    return (
+        <header className="fixed top-0 z-50 w-full px-8 py-2 bg-black border-b-2 border-black bg-opacity-90 border-opacity-10 desktop:px-0">
+            <nav className="flex items-center justify-between h-full mx-auto containerXl">
+                <div className="flex w-auto justify-evenly">
+                    {navbarItems.map((item) => (
+                        <NavbarItem key={item.id} {...item} />
+                    ))}
                 </div>
-            </div>
-        </nav>
-    </header>
-);
+                <div className="flex items-center">
+                    <ToggleLanguage locale={locale} pathname={pathname} />
+                    <div className="flex ml-2 justify-evenly w-28">
+                        <SocialMedia linkClasses="text-xl" />
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+};
 
 export default Navbar;
--- components/NavbarMobile.tsx
+++ components/NavbarMobile.tsx
@@ -2,7 +2,7 @@ import { NavbarItemsProps } from 'types';
 import NavbarItem from './NavbarItem';
 
 const NavbarMobile = ({ navbarItems }: NavbarItemsProps): JSX.Element => (
-    <nav className="fixed bottom-0 z-50 grid w-full grid-cols-4 gap-0 bg-black border-t-2 border-black bg-opacity-20 border-opacity-10">
+    <nav className="fixed bottom-0 z-50 grid w-full grid-cols-4 gap-0 bg-black border-t-2 border-black bg-opacity-90 border-opacity-10">
         {navbarItems.map((item) => (
             <NavbarItem key={item.id} {...item} />
         ))}
