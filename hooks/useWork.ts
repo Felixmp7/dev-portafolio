a14df18f48fcb82bff562422630a7afdc4a726e2
Felix Pacheco <devfex.19@gmail.com>
2021-10-28 14:56:38 -0400

    Refactor useWork

--- hooks/useWork.ts
+++ hooks/useWork.ts
@@ -1,15 +1,9 @@
 import works from 'static-data/works.json';
 
 const useWork = () => {
-    const getWork = (slug: string) => works.find((work) => work.slug === slug);
+    const getWork = (slug: string | string[] | undefined) => works.find((work) => work.slug === slug);
 
-    const getEnterprises = () => works.map((work) => ({
-        name: work.enterprise,
-        slug: work.slug,
-        logo: work.enterpriseIcon,
-    }));
-
-    return { works, getWork, getEnterprises };
+    return { works, getWork };
 };
 
 export default useWork;
