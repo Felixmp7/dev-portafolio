950d2cf3750fb035d1823d1e86bbfb46db0e63a2
Felix Pacheco <devfex.19@gmail.com>
2021-10-28 14:57:13 -0400

    Implement ScreenLoader

---
+++ components/widgets/ScreenLoader.tsx
@@ -0,0 +1,10 @@
+import Layout from '@components/Layout';
+import BarLoader from 'react-spinners/BarLoader';
+
+const ScreenLoader = () => (
+    <Layout extraClasses="h-screen bg-black flex items-center justify-center">
+        <BarLoader color="#17c8f5" loading />
+    </Layout>
+);
+
+export default ScreenLoader;
