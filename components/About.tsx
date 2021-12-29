bfe58a5c422dbae1e5753495505c7d62fd024dbe
Felix Pacheco <devfex.19@gmail.com>
2021-12-29 11:21:27 -0400

    Refactoriza About

--- components/About.tsx
+++ components/About.tsx
@@ -1,29 +1,70 @@
-import AboutCard from './AboutCard';
-import TitleWithLine from './widgets/Title';
-import histories from './static-data/about.json';
-import useIsEnglishLanguage from '../hooks/useIsEnglishLanguage';
+import Image from 'next/image';
+import Title from '@widgets/Title';
+import me from 'public/assets/about/yo2.jpeg';
+import { useTranslation } from 'next-i18next';
 
-const theme = {
-    textColor: 'text-custom-purple',
-    borderColor: 'border-custom-purple',
-};
-
-const About = (): JSX.Element => {
-    const isEnglishLanguage = useIsEnglishLanguage();
+const About = () => {
+    const { t } = useTranslation('common');
+    const {
+        title, description, info, downloadCV,
+    } = t('about', { returnObjects: true });
 
     return (
         <section id="about" className="w-full mx-auto mobileContainer containerXl">
-            <TitleWithLine
-                text={isEnglishLanguage ? 'About' : 'Acerca'}
-                colorText={isEnglishLanguage ? 'me' : 'de mi'}
-                theme={theme}
-            />
-            <div className="grid grid-cols-4 gap-4 mx-auto mt-10 tabletLg:grid-cols-12">
-                {histories.map(((history) => (
-                    <AboutCard key={history.id} {...history} />
-                )))}
+            <Title title={title} color="text-custom-purple" className="pt-20 mb-5" />
+            <div className="grid items-center grid-flow-col gap-20">
+                <Image
+                    src={me}
+                    alt="Felix Pacheco"
+                    width={240}
+                    height={240}
+                    className="rounded-full"
+                />
+                <p className="text-lg font-light leading-relaxed">
+                    {description}
+                </p>
             </div>
+            <div className="grid w-1/2 grid-cols-2 p-6 mx-auto my-10 border-2 rounded gap-x-8 justify-items-center border-custom-purple">
+                <div className="flex flex-col">
+                    <b className="my-1">
+                        {info.nameKey}
+                        <span className="ml-1 font-light">{info.name}</span>
+                    </b>
+                    <b className="my-1">
+                        {info.phoneKey}
+                        <span className="ml-1 font-light">{info.phone}</span>
+                    </b>
+                    <b className="my-1">
+                        {info.frontendExperienceKey}
+                        <span className="ml-1 font-light">{info.frontendExperience}</span>
+                    </b>
+                </div>
+                <div className="flex flex-col">
+                    <b className="my-1">
+                        {info.ageKey}
+                        <span className="ml-1 font-light">{info.age}</span>
+                    </b>
 
+                    <b className="my-1">
+                        {info.addressKey}
+                        <span className="ml-1 font-light">{info.address}</span>
+                    </b>
+                    <b className="my-1">
+                        {info.workModeKey}
+                        <span className="ml-1 font-light">{info.workMode}</span>
+                    </b>
+                </div>
+            </div>
+            <div className="p-2 text-center">
+                <a
+                    href="/files/portafolio.pdf"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="px-6 py-2 text-sm uppercase transition-all duration-500 border border-white rounded-full cursor-pointer hover:bg-custom-purple hover:border-opacity-0 ease"
+                >
+                    {downloadCV}
+                </a>
+            </div>
         </section>
     );
 };
