d560c5bb013782bd0480d96f003cf48c00622149
Felix Pacheco <devfex.19@gmail.com>
2021-12-29 11:21:13 -0400

    Admite className como prop

--- components/widgets/Title.tsx
+++ components/widgets/Title.tsx
@@ -2,10 +2,13 @@ type Props = {
     title?: string | string[];
     description?: string;
     color?: string;
+    className?: string
 }
 
-const Title = ({ title, description, color }: Props): JSX.Element => (
-    <div className="text-center">
+const Title = ({
+    title, description, color, className,
+}: Props): JSX.Element => (
+    <div className={`text-center ${className}`}>
         <h2 className={`text-4xl font-medium mobileMd:text-8xl mobileXl:text-10xl ${color}`}>{title}</h2>
         {description && <span className="font-light text-white mobileMd:text-lg text-opacity-90">{description}</span>}
     </div>
@@ -15,6 +18,7 @@ Title.defaultProps = {
     title: 'Título',
     description: undefined,
     color: 'text-white',
+    className: '',
 };
 
 export default Title;
